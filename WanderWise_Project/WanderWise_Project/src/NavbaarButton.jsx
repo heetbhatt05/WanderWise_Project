14e32b5c1023df9593a9ed2508eb91f4f2e80964
MAHARAJAHEET05 <heetbhatt48@gmail.com>
2025-06-19 08:09:43 +0530

    Updated and deleted unwanted files

--- WanderWise_Project/WanderWise_Project/src/NavbaarButton.jsx
+++
@@ -1,9 +0,0 @@
-import "./NavbaarButton.css";
-
-export default function NavbaarButton() {
-    return (
-        <div>
-            <button className="btn">Start Planning</button>
-        </div>
-    )
-}

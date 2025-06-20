a157c91f4c94be22e6f8ae93e435f90bb1baadf6
MAHARAJAHEET05 <heetbhatt48@gmail.com>
2025-06-20 17:55:48 +0530

    Updated Project

--- src/NavbaarButton.jsx
+++
@@ -1,9 +0,0 @@
-import "./NavbaarButton.css";
-
-export default function NavbaarButton() {
-    return (
-        <div className="main-btn1">
-            <button className="btn1">Start Planning</button>
-        </div>
-    )
-}
